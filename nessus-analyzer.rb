fb633e39c9b5290c95776384b19e422c51dd4c4b
George Ehrhorn <gehrhorn@wayfair.com>
2013-05-17 11:47:03 -0400

    Process all .nessus files in a given directory

--- nessus-analyzer.rb
+++ nessus-analyzer.rb
@@ -3,11 +3,15 @@
 require 'rubygems'
 require 'ruby-nessus'
 
-Nessus::Parse.new("/data/nessus-analyzer-data/ehrhorn.nessus") do |scan|
-  puts "#{scan.title} included #{ scan.hosts.count } hosts"
-  puts "High severity issues #{scan.high_severity_count}"
-  puts "Medium severity issue #{scan.medium_severity_count}"
-  puts "Low severity issuse #{scan.low_severity_count}"
-  puts "Total open ports #{scan.open_ports_count}"
+report_root_dir = "/data/nessus-analyzer-data/"
+
+Dir.glob(report_root_dir+'*.nessus') do |report_file|
+  Nessus::Parse.new(report_file) do |scan|
+    puts "#{scan.title} included #{ scan.hosts.count } hosts"
+    puts "High severity issues #{scan.high_severity_count}"
+    puts "Medium severity issue #{scan.medium_severity_count}"
+    puts "Low severity issuse #{scan.low_severity_count}"
+    puts "Total open ports #{scan.open_ports_count}"
+  end
 end
 
